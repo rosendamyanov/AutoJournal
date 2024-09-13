614ad1a6f99e4032f975ca6c95beb434e8eb62da
Rosen <rosen.damyanov7@gmail.com>
2024-09-14 00:55:33 +0300

    Add project files

---
+++ AutoJournal.Data/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using AutoJournal.Data.Context;
+using AutoJournal.Data.Models;
+
+namespace AutoJournal.Data.Repositories
+{
+    public class UserRepository
+    {
+        private readonly ApplicationContext _context;
+
+        public UserRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Register(User user)
+        {
+            _context.Add(user);
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
